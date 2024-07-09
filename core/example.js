ecc994d7acc8f19968d847c3baa5de51424947e5
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-07-09 16:35:59 +0200

    2 file edited

--- core/example.js
+++ core/example.js
@@ -1,31 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const element = document.getElementById('movable');
-  const mover = new Mover(element);
+const element = document.getElementById('movable');
+const mover = new Mover(element);
 
-  // Raggiungi una posizione casuale
-  //mover.achieve.randomXY();
+// Raggiungi una posizione casuale
+//mover.achieve.randomXY();
 
-  mover.achieve.setXY(0, 0)
+mover.achieve.setXY(0, 0)
 
-  // Raggiungi la posizione del puntatore del mouse al click
-  /*document.addEventListener('click', (event) => {
-    mover.achieve.mouseFromEvent(event);
-  });*/
+// Raggiungi la posizione del puntatore del mouse al click
+/*document.addEventListener('click', (event) => {
+  mover.achieve.mouseFromEvent(event);
+});*/
 
-  // Scivola in 1 secondo alla posizione (200, 200)
-  mover.glideAt(200, 200, 5);
+// Scivola in 1 secondo alla posizione (200, 200)
+mover.glideAt(200, 200, 5);
 
-  // Punta in direzione 45 gradi
-  mover.punteInDirection(90);
+// Punta in direzione 45 gradi
+mover.punteInDirection(90);
 
-  mover.achieve.idElement('')
+mover.achieve.idElement('movable2')
 
-  // Cambia la posizione X di 50 pixel
-  //mover.changeX(50);
+// Cambia la posizione X di 50 pixel
+//mover.changeX(50);
 
-  // Cambia la posizione Y di 50 pixel
-  //mover.changeY(50);
+// Cambia la posizione Y di 50 pixel
+//mover.changeY(50);
 
-  // Raggiungi la posizione del mouse quando viene chiamato
-  //mover.achieve.mouse();
-});
+// Raggiungi la posizione del mouse quando viene chiamato
+//mover.achieve.mouse();
