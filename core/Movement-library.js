e32612db16c8ce85dcb8dc93e6e282cb9c6b6c7c
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-07-09 14:35:50 +0200

    1 file edited

--- core/Movement-library.js
+++ core/Movement-library.js
@@ -4,59 +4,59 @@ class Mover {
     this.element.style.position = 'absolute'; // Assicurati che l'elemento sia posizionato in modo assoluto
   }
     
-  raggiungi = {
+  achieve = {
     XY: (x, y) => {
       this.element.style.left = `${x}px`;
       this.element.style.top = `${y}px`;
     },
 
     randomXY: () => {
-      this.raggiungi.XY((Math.random() * window.innerWidth), (Math.random() * window.innerHeight));
+      this.achieve.XY((Math.random() * window.innerWidth), (Math.random() * window.innerHeight));
     },
 
     idElement: (id) => {
       const target = document.getElementById(id);
       if (target) {
         const rect = target.getBoundingClientRect();
-        this.raggiungi.XY(rect.left + window.scrollX, rect.top + window.scrollY);
+        this.achieve.XY(rect.left + window.scrollX, rect.top + window.scrollY);
       }
     },
 
     mouseFromEvent: (mouseEvent) => {
       const x = mouseEvent.clientX;
       const y = mouseEvent.clientY;
-      this.raggiungi.XY(x, y);
+      this.achieve.XY(x, y);
     },
 
     mouse: () => {
       const moveToMouse = (event) => {
         const x = event.clientX;
         const y = event.clientY;
-        this.raggiungi.XY(x, y);
+        this.achieve.XY(x, y);
       };
       document.addEventListener('mousemove', moveToMouse, { once: true });
     }
   }
 
-  scivolaIn(seconds, x, y) {
+  glideAt(x, y, seconds) {
     this.element.style.transition = `all ${seconds}s`;
-    this.raggiungi.XY(x, y);
+    this.achieve.XY(x, y);
     setTimeout(() => {
       this.element.style.transition = ''; // Rimuove la transizione dopo che è completata
     }, seconds * 1000);
   }
 
-  puntaInDirezione(degrees) {
+  pointeInDirection(degrees) {
     this.element.style.transform = `rotate(${degrees}deg)`;
   }
 
-  cambiaX(dx) {
+  changeX(dx) {
     const currentX = parseInt(this.element.style.left) || 0;
-    this.raggiungi.XY(currentX + dx, parseInt(this.element.style.top) || 0);
+    this.achieve.XY(currentX + dx, parseInt(this.element.style.top) || 0);
   }
 
-  cambiaY(dy) {
+  changeY(dy) {
     const currentY = parseInt(this.element.style.top) || 0;
-    this.raggiungi.XY(parseInt(this.element.style.left) || 0, currentY + dy);
+    this.achieve.XY(parseInt(this.element.style.left) || 0, currentY + dy);
   }
 }
